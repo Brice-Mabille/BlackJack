23d9d7998eb1e546f6f23561c9135fbd3408907d
Brice-Mabille <114212325+Brice-Mabille@users.noreply.github.com>
2025-01-15 23:01:52 +0100

    a

--- carte_jeu.py
+++ carte_jeu.py
@@ -48,11 +48,9 @@ class Hand:
             self.aces += 1  # add to self.aces
 
     def adjust_for_ace(self):
-        while self.value > 21 and self.aces:
+        while self.value > 21 or self.aces:
             self.value -= 10
             self.aces -= 1
-
-
 
 def hit(deck, hand):
     hand.add_card(deck.deal())
